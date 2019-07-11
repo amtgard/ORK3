1f75624527d71a4daa0505d539bb8029c64039af
Ken Walker <ken.walker@bedarra.com>
2019-07-11 12:55:50 -0400

    Inactive chapters are displayed on Kingdom map

--- orkui/controller/controller.Kingdom.php
+++ orkui/controller/controller.Kingdom.php
@@ -46,7 +46,14 @@ class Controller_Kingdom extends Controller {
 	
 	public function map($kingdom_id = null) {
 		if (valid_id($kingdom_id)) {
-			$this->data['Parks'] = $this->Kingdom->GetParks(array('KingdomId' => $kingdom_id));
+      $all_parks = $this->Kingdom->GetParks(array('KingdomId' => $kingdom_id));
+      $all_parks['Parks'] = array_filter(
+        $all_parks['Parks'],
+        function ($park) {
+          return $park['Active'] == 'Active';
+        }
+      );
+      $this->data['Parks'] = $all_parks;
 		}
 	}
 
